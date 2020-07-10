7f483c2dab63641b70e1d53e29374f2cff394816
Seth Michael Larson <sethmichaellarson@gmail.com>
2020-07-10 10:58:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+Writing Python at [`@elastic`](https://github.com/elastic) and maintaining [urllib3](https://github.com/urllib3/urllib3) and related projects.
+Follow [`@sethmlarson` on Twitter](https://twitter.com/sethmlarson) and check out my [tech blog](https://sethmlarson.dev).
+
+<table><tr><td valign="top">
+
+### On my blog
+<!-- blog starts -->
+* [Designing Libraries for Async and Sync I/O](http://sethmlarson.dev/blog/2020-06-27/designing-libraries-for-async-and-sync-io) 2020-06-27
+* [Why URLs are Hard: Path Params & urlparse](http://sethmlarson.dev/blog/2020-04-10/why-urls-are-hard-path-params-urlparse) 2020-04-10
+* [urllib3 in 2020](http://sethmlarson.dev/blog/2020-03-14/urllib3-in-2020) 2020-03-14
+* [Review of 2019 for urllib3](http://sethmlarson.dev/blog/2019-12-28/review-of-2019-for-urllib3) 2019-12-28
+* [HTTP Header Compression](http://sethmlarson.dev/blog/2019-12-06/http-header-compression) 2019-12-06
+<!-- blog ends -->
+More posts on [sethmlarson.dev](https://sethmlarson.dev)
+</td><td valign="top">
+
+### Other content
+<!-- other starts -->
+* **(Upcoming)** [Introduction to the Python Elasticsearch Client](https://community.elastic.co/events/details/elastic-emea-virtual-presents-introduction-into-the-python-elasticsearch-client) 2020-08-05
+* [Introduction to Eland: Explore and analyze data in Elasticsearch with a Pandas-compatible API](https://community.elastic.co/events/details/elastic-emea-virtual-presents-introduction-into-eland-a-python-package-to-analyse-explore-and-manipulate-elasticsearch-data) 2020-07-08
+* [Eland Demo Jupyter Notebook](https://eland.readthedocs.io/en/latest/examples/introduction_to_eland_webinar.html) 2020-07-08
+* [Native async support in Elasticsearch Python client](https://elasticsearch-py.readthedocs.io/en/master/async.html) 2020-06-18
+<!-- other ends -->
+</td></tr></table>
---
+++ build-readme.py
@@ -0,0 +1,79 @@
+import time
+import re
+import feedparser
+
+
+html_braces = re.compile(r"<!-- ([a-z]+) (starts|ends) -->")
+other_content = [
+    {
+        "title": "Introduction to the Python Elasticsearch Client",
+        "link": "https://community.elastic.co/events/details/elastic-emea-virtual-presents-introduction-into-the-python-elasticsearch-client",
+        "published": "2020-08-05"
+    },
+    {
+        "title": "Introduction to Eland: Explore and analyze data in Elasticsearch with a Pandas-compatible API",
+        "link": "https://community.elastic.co/events/details/elastic-emea-virtual-presents-introduction-into-eland-a-python-package-to-analyse-explore-and-manipulate-elasticsearch-data",
+        "published": "2020-07-08"
+    },
+    {
+        "title": "Eland Demo Jupyter Notebook",
+        "link": "https://eland.readthedocs.io/en/latest/examples/introduction_to_eland_webinar.html",
+        "published": "2020-07-08"
+    },
+    {
+        "title": "Native async support in Elasticsearch Python client",
+        "link": "https://elasticsearch-py.readthedocs.io/en/master/async.html",
+        "published": "2020-06-18"
+    }
+]
+
+
+def replace_lines(brace_name, lines, replacement):
+    replaced_lines = []
+    found_start = False
+    found_end = False
+    for line in lines:
+        match = html_braces.match(line.lstrip())
+        if match:
+            if match.groups() == (brace_name, "starts"):
+                found_start = True
+                replaced_lines.append(line)
+                replaced_lines.extend(replacement)
+            elif found_start and match.groups() == (brace_name, "ends"):
+                found_end = True
+        if found_start == found_end:
+            replaced_lines.append(line)
+    assert found_start and found_end
+    return replaced_lines
+
+
+def main():
+    with open("README.md") as f:
+        lines = f.read().split("\n")
+
+    # Reading and updating the blog
+    blog_rss = feedparser.parse("https://sethmlarson.dev/feed")
+    blog_lines = []
+    for entry in blog_rss["entries"]:
+        blog_lines.append(
+            f"* [{entry['title']}]({entry['link']}) "
+            f"{'-'.join(str(x).zfill(2) for x in entry['published_parsed'][:3])}"
+        )
+    lines = replace_lines("blog", lines, blog_lines)
+
+    # Updating other content
+    today = time.gmtime(time.time())[:3]
+    other_lines = []
+    for entry in other_content:
+        is_upcoming = tuple([int(x.lstrip("0")) for x in entry['published'].split("-")]) > today
+        other_lines.append(
+            f"* {'**(Upcoming)** ' if is_upcoming else ''}[{entry['title']}]({entry['link']}) {entry['published']}"
+        )
+    lines = replace_lines("other", lines, other_lines)
+
+    with open("README.md", "w") as f:
+        f.write("\n".join(lines).rstrip() + "\n")
+
+
+if __name__ == "__main__":
+    main()
